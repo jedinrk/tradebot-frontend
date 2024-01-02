8cba05c64e082c186d2a11ea4b29315a7b09ae7e
jedinrk <jedi.nrk@gmail.com>
2024-01-02 12:58:17 +0530

    updated navbar visibility

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,17 +1,31 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function Navbar() {
-  return (
-    <div className="flex h-16">
-      <Link className="m-2" href={"/dashboard"}>
-        Dashboard
-      </Link>
-      <Link className="m-2" href={"/strategies"}>
-        Strategies
-      </Link>
-    </div>
-  );
+  const pathname = usePathname();
+
+  // Check if the current route is '/'
+  const isHomePage = pathname === "/";
+
+  // Render the navbar only if the route is not '/'
+  if (!isHomePage) {
+    return (
+      <div className="flex h-16">
+        <Link className="m-2" href={"/dashboard"}>
+          Dashboard
+        </Link>
+        <Link className="m-2" href={"/strategies"}>
+          Strategies
+        </Link>
+      </div>
+    );
+  }
+
+  // If the route is '/', do not render the navbar
+  return null;
 }
 
 export default Navbar;
